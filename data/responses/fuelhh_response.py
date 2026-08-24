7d9482e0fbe0cc301f3f5e37634e0e924e814066
Konrad H <handkekonrad@gmail.com>
2026-08-24 23:21:57 +0100

    Add fuelhh response for testing

---
+++ data/responses/fuelhh_response.py
@@ -0,0 +1,47 @@
+fuelhh_response = [
+    {
+        'dataset': 'FUELHH', 
+        'publishTime': '2026-08-22T23:30:00Z', 
+        'startTime': '2026-08-22T23:00:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 1, 
+        'fuelType': 'BIOMASS', 
+        'generation': 3222
+    }, 
+    {
+        'dataset': 'FUELHH', 
+        'publishTime': '2026-08-22T23:30:00Z', 
+        'startTime': '2026-08-22T23:00:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 1, 
+        'fuelType': 'CCGT', 
+        'generation': 3475
+    }, 
+    {
+        'dataset': 'FUELHH', 
+        'publishTime': '2026-08-22T23:30:00Z', 
+        'startTime': '2026-08-22T23:00:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 1, 
+        'fuelType': 'COAL', 
+        'generation': 0
+    }, 
+    {
+        'dataset': 'FUELHH', 
+        'publishTime': '2026-08-22T23:30:00Z', 
+        'startTime': '2026-08-22T23:00:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 1, 
+        'fuelType': 'INTELEC', 
+        'generation': 996
+    }, 
+    {
+        'dataset': 'FUELHH', 
+        'publishTime': '2026-08-22T23:30:00Z', 
+        'startTime': '2026-08-22T23:00:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 1, 
+        'fuelType': 'INTEW', 
+        'generation': -532
+    }
+]
