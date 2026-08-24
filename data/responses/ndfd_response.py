6d92c2712f8a9c622d50265b89d3c2d402702770
Konrad H <handkekonrad@gmail.com>
2026-08-25 00:40:04 +0100

    Add ndfd response for testing

---
+++ data/responses/ndfd_response.py
@@ -0,0 +1,26 @@
+ndfd_response = [
+    {
+        'dataset': 'NDFD', 
+        'publishTime': '2026-08-24T13:45:00Z', 
+        'forecastDate': '2026-08-26', 
+        'demand': 28990
+    }, 
+    {
+        'dataset': 'NDFD', 
+        'publishTime': '2026-08-24T13:45:00Z', 
+        'forecastDate': '2026-08-27', 
+        'demand': 29040
+    }, 
+    {
+        'dataset': 'NDFD', 
+        'publishTime': '2026-08-24T13:45:00Z', 
+        'forecastDate': '2026-08-28', 
+        'demand': 26240
+    }, 
+    {
+        'dataset': 'NDFD', 
+        'publishTime': '2026-08-24T13:45:00Z', 
+        'forecastDate': '2026-08-29', 
+        'demand': 23620
+    }
+]
