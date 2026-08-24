d1fcba26dd3affa606f88e402e213c5444d9acdb
Konrad H <handkekonrad@gmail.com>
2026-08-25 00:18:19 +0100

    Add ndf response for testing

---
+++ data/responses/ndf_response.py
@@ -0,0 +1,30 @@
+ndf_response = [
+    {
+        'dataset': 'NDF', 
+        'demand': 25900, 
+        'publishTime': '2026-08-23T18:17:00Z', 
+        'startTime': '2026-08-23T18:30:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 40, 
+        'boundary': 'N'
+    }, 
+    {
+        'dataset': 'NDF', 
+        'demand': 25862, 
+        'publishTime': 
+        '2026-08-23T18:17:00Z', 
+        'startTime': '2026-08-23T19:00:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 41, 
+        'boundary': 'N'
+    }, 
+    {
+        'dataset': 'NDF', 
+        'demand': 25452, 
+        'publishTime': '2026-08-23T18:17:00Z', 
+        'startTime': '2026-08-23T19:30:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 42, 
+        'boundary': 'N'
+    }
+]
