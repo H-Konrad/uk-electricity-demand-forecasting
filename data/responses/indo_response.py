ce4c7305841ca2e3b46b13e4fc14465ab31284d6
Konrad H <handkekonrad@gmail.com>
2026-08-24 22:04:04 +0100

    Add indo response for testing

---
+++ data/responses/indo_response.py
@@ -0,0 +1,26 @@
+indo_response = [
+    {
+        'dataset': 'INDO', 
+        'publishTime': '2026-08-23T19:00:00Z', 
+        'startTime': '2026-08-23T18:30:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 40, 
+        'demand': 26128
+    }, 
+    {
+        'dataset': 'INDO', 
+        'publishTime': '2026-08-23T18:30:00Z', 
+        'startTime': '2026-08-23T18:00:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 39, 
+        'demand': 25852
+    }, 
+    {
+            'dataset': 'INDO', 
+            'publishTime': '2026-08-23T18:00:00Z', 
+            'startTime': '2026-08-23T17:30:00Z', 
+            'settlementDate': '2026-08-23', 
+            'settlementPeriod': 38, 
+        'demand': 25365
+    }
+]
