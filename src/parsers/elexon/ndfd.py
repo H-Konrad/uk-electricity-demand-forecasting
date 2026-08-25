d59f4b46b875ec3d71879b4b4b77f3b201d17c0a
Konrad H <handkekonrad@gmail.com>
2026-08-25 15:59:28 +0100

    Add elexon NDFD parser

---
+++ src/parsers/elexon/ndfd.py
@@ -0,0 +1,6 @@
+def ndf_parser(record):
+    return {
+        "publish_time": record["publishTime"],
+        "forecast_date": record["forecastDate"],
+        "forecast_demand": record["demand"]
+    }
