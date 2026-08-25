1c2c835fcc2099fad99ff98b62ab1228b8937247
Konrad H <handkekonrad@gmail.com>
2026-08-25 18:20:31 +0100

    Add elexon AGWS parser

---
+++ src/parsers/elexon/agws.py
@@ -0,0 +1,7 @@
+def agws_parser(record):
+    return {
+        "publish_time": record["publishTime"],
+        "start_time": record["startTime"],
+        "fuel_type": record["psrType"],
+        "generation": record["demand"]
+    }
