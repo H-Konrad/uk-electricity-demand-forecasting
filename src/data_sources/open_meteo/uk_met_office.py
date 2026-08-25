9ed1dbd83292a577ad7eec18d3f87cbe145f1280
Konrad H <handkekonrad@gmail.com>
2026-08-25 19:54:52 +0100

    Add Open-Meteo UK Met Office API reader

---
+++ src/data_sources/open_meteo/uk_met_office.py
@@ -0,0 +1,55 @@
+import openmeteo_requests
+import requests_cache
+from retry_requests import retry
+
+url = "https://api.open-meteo.com/v1/forecast"
+
+def get_weather_data(
+        latitude, 
+        longitude,
+        start_date,
+        end_date,
+        hourly = [
+            "temperature_2m", 
+            "relative_humidity_2m", 
+            "apparent_temperature",
+            "snowfall",
+            "rain",
+            "showers",
+            "weather_code"
+        ],
+        models = "ukmo_seamless"
+    ):
+    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    openmeteo = openmeteo_requests.Client(session = retry_session)
+
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "hourly": hourly,
+        "models": models,
+        "start_date": start_date,
+        "end_date": end_date,
+    }
+
+    response = openmeteo.weather_api(
+        url = url, 
+        params = params
+    )
+
+    return response
+
+if __name__ == "__main__":
+    latitude = 51.5085
+    longitude = -0.1257
+    start_date = "2026-08-18"
+    end_date = "2026-08-19"
+
+    response = get_weather_data(
+        latitude = latitude,
+        longitude = longitude,
+        start_date = start_date,
+        end_date = end_date
+    )
+
