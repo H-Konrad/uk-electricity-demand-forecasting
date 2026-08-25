6f481220d6e0fad3a40ad898a48cae2efb8e84ff
Konrad H <handkekonrad@gmail.com>
2026-08-26 00:39:14 +0100

    Add Open-Meteo UK Met Office API parser

---
+++ src/parsers/open_meteo/uk_met_office.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+def uk_met_office_parser(response):
+    hourly = response.Hourly()
+
+    hourly_data = {
+        "date": pd.date_range(
+            start = pd.to_datetime(
+                hourly.Time(), 
+                unit = "s", 
+                utc = True
+            ),
+            end = pd.to_datetime(
+                hourly.TimeEnd(), 
+                unit = "s", 
+                utc = True
+            ),
+            freq = pd.Timedelta(
+                seconds = hourly.Interval()
+            ),
+            inclusive = "left"
+        )
+    }
+
+    return {
+        "time": hourly_data["date"],
+        "interval": hourly.Interval(),
+        "temperature_2m": hourly.Variables(0).ValuesAsNumpy(),
+        "relative_humidity_2m": hourly.Variables(1).ValuesAsNumpy(),
+        "apparent_temperature": hourly.Variables(2).ValuesAsNumpy(),
+        "snowfall": hourly.Variables(3).ValuesAsNumpy(),
+        "rain": hourly.Variables(4).ValuesAsNumpy(),
+        "showers": hourly.Variables(5).ValuesAsNumpy(),
+        "weather_code": hourly.Variables(6).ValuesAsNumpy(),
+    }
