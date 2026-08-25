612ef8ed25e4ce907c66731f6b7256469f501b0c
Konrad H <handkekonrad@gmail.com>
2026-08-25 16:40:41 +0100

    Add AGWS response for testing

---
+++ data/responses/agws_response.py
@@ -0,0 +1,38 @@
+agws_response = [
+    {
+        'dataset': 'AGWS', 
+        'documentId': 'NGET-EMFIP-AGWS-0823261830', 
+        'documentRevisionNumber': 1, 
+        'publishTime': '2026-08-23T18:30:04Z', 
+        'businessType': 'Wind generation', 
+        'psrType': 'Wind Onshore', 
+        'quantity': 508.0, 
+        'startTime': '2026-08-23T16:00:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 35
+    }, 
+    {
+        'dataset': 'AGWS', 
+        'documentId': 'NGET-EMFIP-AGWS-0823261830', 
+        'documentRevisionNumber': 1, 
+        'publishTime': '2026-08-23T18:30:04Z', 
+        'businessType': 'Wind generation', 
+        'psrType': 'Wind Offshore', 
+        'quantity': 798.0, 
+        'startTime': '2026-08-23T16:00:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 35
+    }, 
+    {
+        'dataset': 'AGWS', 
+        'documentId': 'NGET-EMFIP-AGWS-0823261830', 
+        'documentRevisionNumber': 1, 
+        'publishTime': '2026-08-23T18:30:04Z', 
+        'businessType': 'Solar generation', 
+        'psrType': 'Solar', 
+        'quantity': 5602.0, 
+        'startTime': '2026-08-23T16:00:00Z', 
+        'settlementDate': '2026-08-23', 
+        'settlementPeriod': 35
+    }
+]
