cda8c633e08f5454dd9590b5b5bb3cdd69fbd96c
Konrad H <handkekonrad@gmail.com>
2026-08-27 23:51:23 +0100

    Add locations for weather data

---
+++ src/data_sources/locations.py
@@ -0,0 +1,50 @@
+locations = [
+    {
+        "location_id": 1,
+        "location_name": "Inverness",
+        "latitude": 57.4777,
+        "longitude": -4.2247
+    },
+    {
+        "location_id": 2,
+        "location_name": "Edinburgh",
+        "latitude": 55.9532,
+        "longitude": -3.1883
+    },
+    {
+        "location_id": 3,
+        "location_name": "Newcastle",
+        "latitude": 54.9666,
+        "longitude": -1.6000
+    },
+    {
+        "location_id": 4,
+        "location_name": "Manchester",
+        "latitude": 53.4839,
+        "longitude": -2.2446
+    },
+    {
+        "location_id": 5,
+        "location_name": "Norwich",
+        "latitude": 52.6308,
+        "longitude": 1.2973
+    },
+    {
+        "location_id": 6,
+        "location_name": "London",
+        "latitude": 51.5074,
+        "longitude": -0.1278
+    },
+    {
+        "location_id": 7,
+        "location_name": "Cardiff",
+        "latitude": 51.4815,
+        "longitude": -3.1790
+    },
+    {
+        "location_id": 8,
+        "location_name": "Plymouth",
+        "latitude": 50.3762,
+        "longitude": -4.1438
+    }
+]
