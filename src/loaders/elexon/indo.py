34e7a93fc002c90529bc2315e9fcd9b8fd389a08
Konrad H <handkekonrad@gmail.com>
2026-08-28 16:59:03 +0100

    Add session parameter to cache api responses

--- src/loaders/elexon/indo.py
+++ src/loaders/elexon/indo.py
@@ -1,18 +1,19 @@
 from datetime import datetime, timedelta
 
+from src.utils.sessions import elexon_session
 from src.data_sources.elexon.indo import get_indo
 from src.parsers.elexon.indo import indo_parser
 from src.database.insert_data import InsertToDatabase
 from src.utils.sessions import elexon_session
 
-def load_indo(start_date, end_date, db, hour_window):
+def load_indo(start_date, end_date, db, hour_window, session):
     start_date = datetime.fromisoformat(start_date.replace("Z", "+00:00"))
     end_date = datetime.fromisoformat(end_date.replace("Z", "+00:00"))
     while start_date < end_date:
         temp_end_date = min(start_date + timedelta(hours = hour_window - 0.5), end_date)
 
         response = get_indo(
-            session = retry_session,
+            session = session,
             publish_date_time_from = start_date.isoformat().replace("+00:00", "Z"),
             publish_date_time_to = temp_end_date.isoformat().replace("+00:00", "Z")
         )
@@ -32,5 +33,6 @@ if __name__ == "__main__":
     #    start_date = "2026-07-01T00:30:00Z",
     #    end_date = "2026-08-01T00:00:00Z",
     #    db = InsertToDatabase(),
-    #    hour_window = 14 * 24
+    #    hour_window = 14 * 24,
+    #    session = retry_session()
     #)
--- src/loaders/elexon/ndf.py
+++ src/loaders/elexon/ndf.py
@@ -1,18 +1,19 @@
 from datetime import datetime, timedelta
 
+from src.utils.sessions import elexon_session
 from src.data_sources.elexon.ndf import get_ndf
 from src.parsers.elexon.ndf import ndf_parser
 from src.database.insert_data import InsertToDatabase
 from src.utils.sessions import elexon_session
 
-def load_ndf(start_date, end_date, db):
+def load_ndf(start_date, end_date, db, session):
     start_date = datetime.fromisoformat(start_date.replace("Z", "+00:00"))
     end_date = datetime.fromisoformat(end_date.replace("Z", "+00:00"))
     while start_date < end_date:
         temp_end_date = min(start_date + timedelta(hours = 0.5), end_date)
 
         response = get_ndf(
-            session = retry_session,
+            session = session,
             publish_date_time_from = start_date.isoformat().replace("+00:00", "Z"),
             publish_date_time_to = temp_end_date.isoformat().replace("+00:00", "Z")
         )
@@ -31,5 +32,6 @@ if __name__ == "__main__":
     #load_ndf(
     #    start_date = "2026-07-01T00:30:00Z",
     #    end_date = "2026-07-02T00:00:00Z",
-    #    db = InsertToDatabase()
+    #    db = InsertToDatabase(),
+    #    session = retry_session()
     #)
