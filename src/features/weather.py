f59510fa784ae723f890174888a152d44b5d54f6
Konrad H <handkekonrad@gmail.com>
2026-09-03 21:18:02 +0100

    Add function to add weather features

--- src/features/weather.py
+++ src/features/weather.py
@@ -21,3 +21,36 @@ def pivot_weather(weather):
     ]
 
     return weather_pivot
+
+def add_weather_features(weather_pivot):
+    temperature_columns = [
+        column for column in weather_pivot.columns
+        if column.startswith("temp")
+    ]
+
+    rain_columns = [
+        col for col in weather_pivot.columns
+        if col.startswith("rain")
+    ]
+
+    snow_columns = [
+        col for col in weather_pivot.columns
+        if col.startswith("snow")
+    ]
+
+    weather_pivot["temperature_mean"] = weather_pivot[temperature_columns].mean(axis = 1)
+    weather_pivot["temperature_min"] = weather_pivot[temperature_columns].min(axis = 1)
+    weather_pivot["temperature_max"] = weather_pivot[temperature_columns].max(axis = 1)
+    weather_pivot = weather_pivot.drop(columns = temperature_columns)
+
+    weather_pivot["cities_with_rain"] = (weather_pivot[rain_columns] > 0).sum(axis = 1)
+    weather_pivot["cities_with_snow"] = (weather_pivot[snow_columns] > 0).sum(axis = 1)
+
+    weather_pivot[rain_columns] = np.log1p(weather_pivot[rain_columns])
+    weather_pivot[snow_columns] = np.log1p(weather_pivot[snow_columns])
+
+    weather_pivot = weather_pivot.rename(columns = {
+        "forecast_time_": "target_time"
+    })
+
+    return weather_pivot
