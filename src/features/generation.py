c94b6f5aa1398b59ddea7487a0828e5719168a67
Konrad H <handkekonrad@gmail.com>
2026-09-03 21:04:00 +0100

    Add function adding generation features

--- src/features/generation.py
+++ src/features/generation.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 fuels = [
     "BIOMASS",
@@ -65,5 +66,32 @@ def pivot_generation(generation):
         values = "generation_mw",
         aggfunc = "last"
     ).reset_index().sort_values("publish_time").drop(columns = "OIL")
+
+    return generation_pivot
+
+def add_generation_features(generation_pivot):
+    generation_pivot["total_fuel"] = generation_pivot[fuels].sum(axis = 1)
+    generation_pivot["total_interconnector"] = generation_pivot[interconnectors].sum(axis = 1)
+    generation_pivot["total_generation"] = generation_pivot["total_fuel"] + generation_pivot["total_interconnector"]
+
+    generation_pivot["OTHER"] = np.log1p(generation_pivot["OTHER"])
+    generation_pivot["OCGT"] = np.log1p(generation_pivot["OCGT"])
+    generation_pivot["NPSHYD"] = np.log1p(generation_pivot["NPSHYD"])
+    generation_pivot["COAL"] = np.log1p(generation_pivot["COAL"])
+    generation_pivot["CCGT"] = np.log1p(generation_pivot["CCGT"])
+
+    generation_pivot["INTNSL"] = np.sign(generation_pivot["INTNSL"]) * np.log1p(np.abs(generation_pivot["INTNSL"]))
+    generation_pivot["INTNEM"] = np.sign(generation_pivot["INTNEM"]) * np.log1p(np.abs(generation_pivot["INTNEM"]))
+
+    features_for_lag = fuels + interconnectors + ["total_fuel", "total_interconnector", "total_generation"]
+
+    for feature in features_for_lag:
+        generation_pivot[f"{feature}_lag_30m"] = generation_pivot[feature].shift(1)
+        generation_pivot[f"{feature}_lag_1h"] = generation_pivot[feature].shift(2)
+        generation_pivot[f"{feature}_lag_2h"] = generation_pivot[feature].shift(4)
+
+    generation_pivot = generation_pivot.rename(columns = {
+        "publish_time": "reference_time"
+    })
 
     return generation_pivot
