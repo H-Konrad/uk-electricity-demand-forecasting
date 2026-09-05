c8289188afd40a01c1cf6f3f44fa2b1d78ca9b11
Konrad H <handkekonrad@gmail.com>
2026-09-05 20:45:22 +0100

    Update models using pipelines

--- src/training/model.py
+++ src/training/model.py
@@ -1,18 +1,37 @@
 from sklearn.linear_model import Ridge
 from sklearn.ensemble import RandomForestRegressor
 from xgboost import XGBRegressor
+from sklearn.pipeline import Pipeline
+from sklearn.base import clone
 
-def load_ridge():
-    return Ridge()
+def model_pipeline(model, preprocessor):
+    pipeline = Pipeline([
+        (
+            "preprocessor",
+            clone(preprocessor)
+        ),
+        (
+            "model",
+            model
+        )
+    ])
 
-def load_random_forest():
-    return RandomForestRegressor(
+    return pipeline
+
+def load_ridge(preprocessor):
+    model = Ridge()
+    return model_pipeline(model, preprocessor)
+
+def load_random_forest(preprocessor):
+    model = RandomForestRegressor(
         random_state = 42,
         n_jobs = -1
     )
+    return model_pipeline(model, preprocessor)
 
-def load_xgboost():
-    return XGBRegressor(
+def load_xgboost(preprocessor):
+    model = XGBRegressor(
         random_state = 42,
         n_jobs = -1
     )
+    return model_pipeline(model, preprocessor)
