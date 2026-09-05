b11e62152d2b20b33a0b540336ef091ed2559a3d
Konrad H <handkekonrad@gmail.com>
2026-09-05 20:54:46 +0100

    Add model evaluation

--- src/training/evaluation.py
+++ src/training/evaluation.py
@@ -16,3 +16,21 @@ def evaluate_baseline(df, prediction_column):
         "mae": mae,
         "rmse": rmse
     }
+
+def evaluate_models(model, X, y):
+    predictions = model.predict(X)
+
+    mae = mean_absolute_error(
+        y_true = y,
+        y_pred = predictions
+    )
+
+    rmse = root_mean_squared_error(
+        y_true = y,
+        y_pred = predictions
+    )
+
+    return {
+        "mae": mae,
+        "rmse": rmse
+    }
