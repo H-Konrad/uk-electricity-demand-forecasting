a24ef14879110ef5b1dc2b7469a666341a86db47
Konrad H <handkekonrad@gmail.com>
2026-09-05 22:23:54 +0100

    Update initial models

--- src/training/models.py
+++ src/training/models.py
@@ -24,6 +24,8 @@ def load_ridge(preprocessor):
 
 def load_random_forest(preprocessor):
     model = RandomForestRegressor(
+        n_estimators = 200,
+        max_depth = 10,
         random_state = 42,
         n_jobs = -1
     )
@@ -31,7 +33,11 @@ def load_random_forest(preprocessor):
 
 def load_xgboost(preprocessor):
     model = XGBRegressor(
-        random_state = 42,
-        n_jobs = -1
+        n_estimators = 200,
+        max_depth = 6,
+        learning_rate = 0.05,
+        device = "cuda",
+        tree_method = "hist",
+        random_state = 42
     )
     return model_pipeline(model, preprocessor)
