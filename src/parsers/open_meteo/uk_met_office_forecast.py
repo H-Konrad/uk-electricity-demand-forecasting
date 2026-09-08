9cead37f7b93daf1a228facc51dc6782ba1185df
Konrad H <handkekonrad@gmail.com>
2026-09-08 22:20:20 +0100

    Add parser for UK Met Office forecast

---
+++ src/parsers/open_meteo/uk_met_office_forecast.py
@@ -0,0 +1,35 @@
+import pandas as pd
+
+def uk_met_office_forecast_parser(
+        location_id,
+        response
+    ):
+    hourly = response.Hourly()
+
+    hourly_data = {
+        "date": pd.date_range(
+            start = pd.to_datetime(
+                hourly.Time(), 
+                unit = "s", 
+                utc = True
+            ),
+            end = pd.to_datetime(
+                hourly.TimeEnd(), 
+                unit = "s", 
+                utc = True
+            ),
+            freq = pd.Timedelta(
+                seconds = hourly.Interval()
+            ),
+            inclusive = "left"
+        )
+    }
+
+    return pd.DataFrame({
+        "location_id": location_id,
+        "forecast_time": hourly_data["date"],
+        "temperature_2m": hourly.Variables(0).ValuesAsNumpy().round(2),
+        "relative_humidity_2m": hourly.Variables(1).ValuesAsNumpy().round(2),
+        "snowfall": hourly.Variables(2).ValuesAsNumpy().round(2),
+        "rain": hourly.Variables(3).ValuesAsNumpy().round(2)
+    })
